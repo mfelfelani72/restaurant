caedef452398f92e602e6f9c37b2d8b706ccf8a6
mfelfelani72@gmail.com <mfcompany21@gmail.com>
2024-03-07 12:55:03 +0400

    testmonial

--- frontend/src/Pages/theme/default/LandingPage.jsx
+++ frontend/src/Pages/theme/default/LandingPage.jsx
@@ -5,6 +5,7 @@ import Menu from "../../../components/theme/default/Menu";
 import CarouselTransition from '../../../components/theme/default/CarouselTransition';
 import CardLandingPage from '../../../components/theme/default/CardLandingPage';
 import BanerLandingPage from '../../../components/theme/default/BanerLandingPage';
+import TestmonialLandingPage from '../../../components/theme/default/TestmonialLandingPage';
 
 function LandingPage() {
     return (
@@ -21,6 +22,8 @@ function LandingPage() {
                 <CardLandingPage />
 
                 <BanerLandingPage />
+
+                <TestmonialLandingPage />
 
 
             </div>
